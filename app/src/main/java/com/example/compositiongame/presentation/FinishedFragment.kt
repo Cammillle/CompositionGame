eb230ed10d0bb7048312c3dd31d3373de39729eb
k.mordvinova <mordvinovacamilla@gmail.com>
2025-06-18 14:39:05 +0300

    Реализация GameFinishedFragment

--- app/src/main/java/com/example/compositiongame/presentation/FinishedFragment.kt
+++ app/src/main/java/com/example/compositiongame/presentation/FinishedFragment.kt
@@ -1,11 +1,13 @@
 package com.example.compositiongame.presentation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.lifecycle.ViewModelProvider
 import com.example.compositiongame.R
 import com.example.compositiongame.databinding.FragmentFinishedBinding
 import com.example.compositiongame.domain.entity.GameResult
@@ -13,7 +15,7 @@ import com.example.compositiongame.domain.entity.GameResult
 
 class FinishedFragment : Fragment() {
     private lateinit var binding: FragmentFinishedBinding
-    private var gameResult: GameResult? = null
+    private lateinit var gameResult:GameResult
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,15 +34,61 @@ class FinishedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                retryGame()
-            }
-        })
+        bindViews()
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    retryGame()
+                }
+            })
         binding.buttonRetry.setOnClickListener {
             retryGame()
         }
     }
+
+    @SuppressLint("StringFormatMatches")
+    private fun bindViews() {
+        with(binding) {
+            emojiResult.setImageResource(getSmileResId())
+            tvRequiredAnswers.text = String.format(
+                getString(R.string.required_score),
+                gameResult.gameSettings.minCountOfRightAnswers
+            )
+            tvScoreAnswers.text = String.format(
+                getString(R.string.score_answers),
+                gameResult.countOfRightAnswers
+            )
+            tvRequiredPercentage.text = String.format(
+                getString(R.string.required_percentage),
+                gameResult.gameSettings.minPercentOfRightAnswers
+            )
+            tvScorePercentage.text = String.format(
+                getString(R.string.score_percentage),
+                getPercentOfRightAnswers()
+            )
+        }
+    }
+
+    private fun getSmileResId(): Int {
+        return if (gameResult.winner) {
+            R.drawable.ic_smile
+        } else {
+            R.drawable.ic_sad
+        }
+    }
+
+    private fun getPercentOfRightAnswers() = with(gameResult) {
+        if (countOfQuestions == 0) {
+            0
+        } else {
+            ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+        }
+    }
+
 
     private fun retryGame() {
         requireActivity().supportFragmentManager.popBackStack(ChooseLevelFragment.NAME, 0)
