d28e9a397bb8e275304d75008fec3ceba0c96355
k.mordvinova <mordvinovacamilla@gmail.com>
2025-06-20 14:06:25 +0300

    Jetpack Navigation dataBinding

--- app/src/main/java/com/example/compositiongame/presentation/BindingAdapters.kt
+++ app/src/main/java/com/example/compositiongame/presentation/BindingAdapters.kt
@@ -1,43 +1,69 @@
 package com.example.compositiongame.presentation
 
 import android.annotation.SuppressLint
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.compositiongame.R
+import com.example.compositiongame.domain.entity.GameResult
 
 @SuppressLint("StringFormatMatches")
 @BindingAdapter("requiredAnswers")
-fun bindRequiredAnswers(textView: TextView, count:Int) {
+fun bindRequiredAnswers(textView: TextView, count: Int) {
     textView.text = String.format(
         textView.context.getString(R.string.required_score),
         count
     )
 }
+
 @SuppressLint("StringFormatMatches")
-@BindingAdapter("scoreAnswers")
-fun bindTvScoreAnswers(textView: TextView, count:Int) {
+@BindingAdapter("score")
+fun bindScore(textView: TextView, score: Int) {
     textView.text = String.format(
         textView.context.getString(R.string.score_answers),
-        count
-    )
-}
-@SuppressLint("StringFormatMatches")
-@BindingAdapter("requiredPercentage")
-fun bindTvRequiredPercentage(textView: TextView, count:Int) {
-    textView.text = String.format(
-        textView.context.getString(R.string.required_percentage),
-        count
-    )
-}
-@SuppressLint("StringFormatMatches")
-@BindingAdapter("scorePercentage")
-fun bindTvScorePercentage(textView: TextView, count:Int) {
-    textView.text = String.format(
-        textView.context.getString(R.string.score_percentage),
-        count
+        score
     )
 }
 
+@SuppressLint("StringFormatMatches")
+@BindingAdapter("requiredPercentage")
+fun bindRequiredPercentage(textView: TextView, percentage: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.required_percentage),
+        percentage
+    )
+}
+
+@SuppressLint("StringFormatMatches")
+@BindingAdapter("scorePercentage")
+fun bindScorePercentage(textView: TextView, gameResult: GameResult) {
+    textView.text = String.format(
+        textView.context.getString(R.string.score_percentage),
+        getPercentOfRightAnswers(gameResult)
+    )
+}
+
+private fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult) {
+    if (countOfQuestions == 0) {
+        0
+    } else {
+        ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+    }
+}
+
+@BindingAdapter("resultEmoji")
+fun bindResultEmoji(imageView: ImageView, winner: Boolean) {
+    imageView.setImageResource(getSmileResId(winner))
+}
+
+private fun getSmileResId(winner: Boolean): Int {
+    return if (winner) {
+        R.drawable.ic_smile
+    } else {
+        R.drawable.ic_sad
+    }
+}
 
 
 
